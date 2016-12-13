4850ad148c69723779f20cda22335127e6f34cda
DougW <dworsham@library.ucla.edu>
2016-12-13 14:15:25 -0800

    adding course in a box

--- js/category-gallery.js
+++
@@ -1,9 +0,0 @@
-(function(discourseGallery){
-    'use strict';
-
-    discourseGallery.loadCategoryGallery({
-        forumUrl: window.forumUrl,
-        category: window.galleryCategory,
-        containerId: 'category-gallery'
-    });
-})(window.discourseGallery);
